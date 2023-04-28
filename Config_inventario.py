710bb8e44dedd534420bdad1148a71f8408fe3ea
jnaranjo <jnaranjo@clicklease.com>
2023-04-27 18:59:21 -0600

    latest version with english comments

--- Config_inventario.py
+++ Config_inventario.py
@@ -1,26 +1,94 @@
 import openpyxl
 import os
+import random
 from prettytable import PrettyTable
+from openpyxl.utils import column_index_from_string
 
-# Load inventory data from Excel file
-inventory_file = "DBs/Inventario.xlsx"
-try:
-    inventory_workbook = openpyxl.load_workbook(inventory_file)
-    inventory_sheet = inventory_workbook.active
-    inventory = {}
-    header_row = inventory_sheet[1]
-    for row in inventory_sheet.iter_rows(min_row=2, values_only=True):
-        asset_id = row[0]
-        asset_details = {}
-        for i in range(1, len(header_row)):
-            header = header_row[i].value
-            asset_details[header] = row[i]
-        inventory[asset_id] = asset_details
-except FileNotFoundError:
-    inventory_workbook = openpyxl.Workbook()
-    inventory_sheet = inventory_workbook.active
-    inventory_sheet.append(["ID"])
-    inventory = {}
+
+
+
+def añadir_equipo():
+    # Open the Excel file
+    wb = openpyxl.load_workbook('DBs/Inventario.xlsx')
+
+    # Select the active worksheet
+    ws = wb.active
+
+    # Find the last row of data in the worksheet
+    last_row = ws.max_row
+
+    # Find the last column of data in the worksheet
+    last_col = ws.max_column
+
+    # Create a list of column headings
+    headings = []
+    for col in range(1, last_col+1):
+        value = ws.cell(row=1, column=col).value
+        if value is not None:
+            headings.append(value)
+
+    # Generate a random and unique ID
+    asset_id = random.randint(1000, 9999)
+    id_column = column_index_from_string('A')
+    used_ids = [ws.cell(row=row, column=id_column).value for row in range(2, last_row+1)]
+    while asset_id in used_ids:
+        asset_id = random.randint(1000, 9999)
+
+    # Create a new row with the generated ID
+    new_row = [str(asset_id)]
+    for heading in headings[1:]:
+        value = input(f"Enter a value for {heading}: ")
+        new_row.append(value)
+
+    # Add the new row to the worksheet
+    ws.append(new_row)
+
+    # Save the changes to the Excel file
+    wb.save('DBs/Inventario.xlsx')
+
+    print(f"Asset {asset_id} added to inventory")
+
+
+
+
+
+def eliminar_equipo():
+    """Deletes an asset from the inventory"""
+    inventory_file = "DBs/Inventario.xlsx"
+    try:
+        wb = openpyxl.load_workbook(inventory_file)
+        ws = wb.active
+        inventory = {}
+        header_row = ws[1]
+        for row in ws.iter_rows(min_row=2, values_only=True):
+            asset_id = row[0]
+            asset_details = {}
+            for i in range(1, len(header_row)):
+                header = header_row[i].value
+                asset_details[header] = row[i]
+            inventory[asset_id] = asset_details
+    except FileNotFoundError:
+        print("Inventory file not found!")
+        return
+
+    asset_id = input("Enter ID to delete: ")
+    if asset_id in inventory:
+        del inventory[asset_id]  # Remove the item from the dictionary directly
+        asset_row = None
+        for i, row in enumerate(ws.rows):
+            if row[0].value == asset_id:
+                asset_row = i + 1
+                break
+        if asset_row is not None:
+            ws.delete_rows(asset_row)
+            wb.save(inventory_file)
+            print(f"Asset {asset_id} deleted from inventory")
+        else:
+            print(f"Asset {asset_id} not found in inventory")
+    else:
+        print(f"Asset {asset_id} not found in inventory")
+    print()
+
 
 
 
@@ -51,92 +119,37 @@ def ver_inventario():
     input("\nPress Enter to continue...\n")
 
 
+
 def exportar_inventario():
     """Exports the inventory to an Excel file"""
-    inventory_workbook = openpyxl.Workbook()
-    inventory_worksheet = inventory_workbook.active
-    inventory_worksheet.title = "Inventario"
+    # Load the Excel file
+    try:
+        wb = openpyxl.load_workbook('DBs/Inventario.xlsx')
+        ws = wb.active
+    except FileNotFoundError:
+        print("Inventory file not found!")
+        return
 
-    inventory_worksheet.cell(row=1, column=1, value="ID")
-    header_row = inventory_sheet[1]
-    for i in range(1, len(header_row)):
-        header = header_row[i].value
-        inventory_worksheet.cell(row=1, column=i+1, value=header)
+    # Create a new workbook for the export
+    export_wb = openpyxl.Workbook()
+    export_ws = export_wb.active
+    export_ws.title = "Inventario"
 
-    for row, (asset_id, asset_details) in enumerate(inventory.items(), start=2):
-        inventory_worksheet.cell(row=row, column=1, value=asset_id)
-        for i in range(1, len(header_row)):
-            header = header_row[i].value
-            inventory_worksheet.cell(row=row, column=i+1, value=asset_details.get(header, ""))
+    # Write the header row
+    header_row = [cell.value for cell in ws[1]]
+    for col, header in enumerate(header_row, start=1):
+        export_ws.cell(row=1, column=col, value=header)
 
+    # Write the data rows
+    for row, data in enumerate(ws.iter_rows(min_row=2, values_only=True), start=2):
+        for col, value in enumerate(data, start=1):
+            export_ws.cell(row=row, column=col, value=value)
 
-    custom_name = input("Enter a custom name for the file: ")
-    filename = f"{custom_name}.xlsx"
-    inventory_file = os.path.join(os.getcwd(), filename)
-    inventory_workbook.save(inventory_file)
-    print(f"Inventory exported to {inventory_file}")
+    # Get the export filename
+    export_filename = input("Enter the export filename: ") + '.xlsx' # Added the extension
+    export_file = os.path.join(os.getcwd(), export_filename)
 
-
-
-
-def añadir_equipo():
-    # Open the Excel file
-    wb = openpyxl.load_workbook('DBs/Inventario.xlsx')
-
-    # Select the active worksheet
-    ws = wb.active
-
-    # Find the last row of data in the worksheet
-    last_row = ws.max_row
-
-    # Find the last column of data in the worksheet
-    last_col = ws.max_column
-
-    # Create a list of column headings
-    headings = []
-    for col in range(1, last_col+1):
-        value = ws.cell(row=1, column=col).value
-        if value is not None:
-            headings.append(value)
-
-    # Create a new row
-    new_row = []
-    for heading in headings:
-        value = input(f"Enter a value for {heading}: ")
-        new_row.append(value)
-        if heading == "ID":
-            asset_id = value
-
-    # Add the new row to the worksheet
-    for col, value in enumerate(new_row, start=1):
-        ws.cell(row=last_row+1, column=col, value=value)
-
-    # Save the changes to the Excel file
-    wb.save('DBs/Inventario.xlsx')
-
-    print(f"Asset {asset_id} added to inventory")
-
-
-def eliminar_equipo():
-    """Deletes an asset from the inventory"""
-    asset_id = input("Enter ID to delete: ")
-    if asset_id in inventory:
-        asset_row = None
-        for i, row in enumerate(inventory_sheet.rows):
-            if row[0].value == asset_id:
-                asset_row = i + 1
-                break
-        if asset_row is not None:
-            inventory_sheet.delete_rows(asset_row)
-            inventory_workbook.save(inventory_file)
-            del inventory[asset_id]
-            print(f"Asset {asset_id} deleted from inventory")
-        else:
-            print(f"Asset {asset_id} not found in inventory")
-    else:
-        print(f"Asset {asset_id} not found in inventory")
-    print()
-
-
-
+    # Save the export file
+    export_wb.save(export_file)
+    print(f"Inventory exported to {export_file}")
 
